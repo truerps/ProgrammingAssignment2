be5fd90721c5453a98292179e96a4e0dea8b20e0
truerps <truerps@gmail.com>
2015-03-22 14:38:01 +0300

    homework is done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix(matrix) produces a list
+# this list incapsulates accessor methods for getting and setting values in a particular environment
+makeCacheMatrix <- function(x = matrix()) 
+{
+  #clean cached value of matrix inversion 
+  solveValue <- NULL
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  # setter 
+  setValue <- function(matrixValue)
+  {
+    #store new value
+    x <<- matrixValue
+    #clean cached value of matrix inversion in outer scope
+    solveValue <<- NULL
+  }
+  # getter 
+  getValue <- function()
+  { 
+    x
+  }
+  setSolveResult <- function(val)
+  {
+    solveValue <<- val
+  }
+  getSolveResult <- function()
+  {
+    solveValue
+  }
+  list( set = setValue, get = getValue, setResult = setSolveResult, getResult = getSolveResult)
 }
 
 
-## Write a short comment describing this function
+## This functions extends standard implementation of solve (matrix inversion) by providing ability to cache inversion results in an intermediate store
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  result <- x$getResult()
+  if(is.null(result))
+  {
+    result <- solve(x$get())
+    x$setResult(result)
+  }
+  result
 }
